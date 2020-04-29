2f54a2d976ec1556b4e59057a8b44d06be69ee6f
Enrico Rossini <enrico.rossini.uk@live.com>
2020-04-29 17:07:17 +0100

    Solution for F# Azure Functions

---
+++ FSharpAzureFunctions/GetOrganizations.fs
@@ -0,0 +1,21 @@
+﻿namespace FunctionApp1
+
+open System
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
+open System;
+open System.IO;
+open System.Threading.Tasks;
+open Microsoft.AspNetCore.Mvc;
+open Microsoft.Azure.WebJobs;
+open Microsoft.Azure.WebJobs.Extensions.Http;
+open Microsoft.AspNetCore.Http;
+open Microsoft.Extensions.Logging;
+
+module GetOrganizations =
+    [<FunctionName("GetOrganizations")>]
+    let Run ([<HttpTrigger(AuthorizationLevel.Function, [|"get"|])>] req: HttpRequest) (log: ILogger) = 
+        async {
+            return "some result"
+        }
+        |> Async.StartAsTask
